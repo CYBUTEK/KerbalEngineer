80cf26461d8cc54ed3d05e77463c6ddfb7bf8199
cybutek <admin@cybutek.net>
2015-05-02 00:54:47 +0100

    Prep 1.0.16.5

--- KerbalEngineer/EngineerGlobals.cs
+++ KerbalEngineer/EngineerGlobals.cs
@@ -33,7 +33,7 @@ namespace KerbalEngineer
         /// <summary>
         ///     Current version of the Kerbal Engineer assembly.
         /// </summary>
-        public const string AssemblyVersion = "1.0.16.4";
+        public const string AssemblyVersion = "1.0.16.5";
 
         #endregion
 
