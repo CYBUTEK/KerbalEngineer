c28a3db984cb79f536f1f5f05e899afa0078baf7
harryyoung <hendrik.gerritzen@googlemail.com>
2015-10-31 22:43:33 +0100

    removed an unintended this.HelpString-line

--- KerbalEngineer/Flight/Readouts/Orbital/ManoeuvreNode/PostBurnApoapsis.cs
+++ KerbalEngineer/Flight/Readouts/Orbital/ManoeuvreNode/PostBurnApoapsis.cs
@@ -37,7 +37,6 @@ namespace KerbalEngineer.Flight.Readouts.Orbital.ManoeuvreNode
             this.Name = "Post-burn Apoapsis";
             this.Category = ReadoutCategory.GetCategory("Orbital");
             this.HelpString = "Farthest point of the vessel's ofbit after the burn.";
-            this.HelpString = String.Empty;
             this.IsDefault = false;
         }
 
