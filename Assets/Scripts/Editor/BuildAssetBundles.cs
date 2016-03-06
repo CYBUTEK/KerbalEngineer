c02d956d58f837f518ea48a0b36d658e6f03993c
cybutek <admin@cybutek.net>
2016-03-06 01:06:36 +0000

    Downgraded Unity project to 5.2.4f1, implemented AssetBundle handling and moved some files about to better locations.

---
+++ Assets/Scripts/Editor/BuildAssetBundles.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class BuildAssetBundles : MonoBehaviour
+{
+    [MenuItem("Assets/Build Asset Bundles")]
+    public static void Build()
+    {
+        BuildPipeline.BuildAssetBundles(Application.dataPath + "/../Output/KerbalEngineer", BuildAssetBundleOptions.UncompressedAssetBundle);
+    }
+}
---
+++ KerbalEngineer/AssetBundleLoader.cs
@@ -0,0 +1,41 @@
+﻿namespace KerbalEngineer
+{
+    using UnityEngine;
+
+    [KSPAddon(KSPAddon.Startup.Instantly, false)]
+    public class AssetBundleLoader : MonoBehaviour
+    {
+        private static AssetBundle m_Images;
+        private static AssetBundle m_Prefabs;
+
+        /// <summary>
+        ///     Gets the loaded images asset bundle.
+        /// </summary>
+        public static AssetBundle images
+        {
+            get
+            {
+                return m_Images;
+            }
+        }
+
+        /// <summary>
+        ///     Gets the loaded prefabs asset bundle.
+        /// </summary>
+        public static AssetBundle prefabs
+        {
+            get
+            {
+                return m_Prefabs;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            string bundlePath = EngineerGlobals.AssemblyPath;
+
+            m_Images = AssetBundle.CreateFromFile(bundlePath + "/images");
+            m_Prefabs = AssetBundle.CreateFromFile(bundlePath + "/prefabs");
+        }
+    }
+}
