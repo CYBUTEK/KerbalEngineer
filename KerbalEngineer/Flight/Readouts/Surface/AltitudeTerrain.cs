cd71581c656994e1d447b9a8c112e6993049a00d
cybutek <admin@cybutek.net>
2016-04-02 14:46:21 +0100

    Altitude (terrain) readout now shows the altitude above underwater terrain when splashed down.

--- KerbalEngineer/Flight/Readouts/Surface/AltitudeTerrain.cs
+++ KerbalEngineer/Flight/Readouts/Surface/AltitudeTerrain.cs
@@ -1,7 +1,7 @@
 ﻿// 
 //     Kerbal Engineer Redux
 // 
-//     Copyright (C) 2014 CYBUTEK
+//     Copyright (C) 2016 CYBUTEK
 // 
 //     This program is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
@@ -12,55 +12,36 @@
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //     GNU General Public License for more details.
-// 
 //     You should have received a copy of the GNU General Public License
 //     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
-
-#region Using Directives
-
-#endregion
+//  
 
 namespace KerbalEngineer.Flight.Readouts.Surface
 {
-    #region Using Directives
-
     using Extensions;
     using Sections;
-
-    #endregion
+    using Vessel = global::Vessel;
 
     public class AltitudeTerrain : ReadoutModule
     {
-        #region Constructors
-
         public AltitudeTerrain()
         {
-			this.Name = "Altitude (Terrain)";
-            this.Category = ReadoutCategory.GetCategory("Surface");
-            this.HelpString = "Shows the vessel's altitude above the terrain. Turns off if splashed.";
-            this.IsDefault = true;
+            Name = "Altitude (Terrain)";
+            Category = ReadoutCategory.GetCategory("Surface");
+            HelpString = "Shows the vessel's altitude above the terrain and water's surface, or altitude above underwater terrain whilst splashed down.";
+            IsDefault = true;
         }
-
-        #endregion
-
-        #region Methods
 
         public override void Draw(SectionModule section)
-		{
-			if (ScienceUtil.GetExperimentSituation (FlightGlobals.ActiveVessel) != ExperimentSituations.SrfSplashed) 
-			{
-				if (FlightGlobals.ActiveVessel.terrainAltitude > 0.0) 
-				{
-					this.DrawLine ((FlightGlobals.ship_altitude - FlightGlobals.ActiveVessel.terrainAltitude).ToDistance (), section.IsHud);
-				} 
-				else 
-				{
-					this.DrawLine ((FlightGlobals.ship_altitude).ToDistance (), section.IsHud);
-				}
-			}
+        {
+            if (FlightGlobals.ActiveVessel.terrainAltitude > 0.0 || FlightGlobals.ActiveVessel.situation == Vessel.Situations.SPLASHED)
+            {
+                DrawLine((FlightGlobals.ship_altitude - FlightGlobals.ActiveVessel.terrainAltitude).ToDistance(), section.IsHud);
+            }
+            else
+            {
+                DrawLine((FlightGlobals.ship_altitude).ToDistance(), section.IsHud);
+            }
         }
-
-        #endregion
     }
 }
