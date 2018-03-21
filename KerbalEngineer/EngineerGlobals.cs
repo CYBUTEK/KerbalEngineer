d7ed9ed08193917c43c427a7e97a769f402c9891
jrbudda <jrbudda@gmail.com>
2018-03-20 23:34:11 -0400

    Fix VAB calculations of engine plates and decouplers.

--- KerbalEngineer/EngineerGlobals.cs
+++ KerbalEngineer/EngineerGlobals.cs
@@ -21,7 +21,7 @@ namespace KerbalEngineer
         /// <summary>
         /// Current version of the Kerbal Engineer assembly.
         /// </summary>
-        public const string ASSEMBLY_VERSION = "1.1.3.0";
+        public const string ASSEMBLY_VERSION = "1.1.4.0";
 
         private static string assemblyFile;
         private static string assemblyName;
