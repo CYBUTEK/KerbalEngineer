ffc21de979b1aaf7bb5a43db9ea9ae7a5dd11fac
jrbudda <jrbudda@gmail.com>
2018-04-11 20:03:07 -0400

    version bump

--- KerbalEngineer/EngineerGlobals.cs
+++ KerbalEngineer/EngineerGlobals.cs
@@ -21,7 +21,7 @@ namespace KerbalEngineer
         /// <summary>
         /// Current version of the Kerbal Engineer assembly.
         /// </summary>
-        public const string ASSEMBLY_VERSION = "1.1.4.8";
+        public const string ASSEMBLY_VERSION = "1.1.4.9";
 
         private static string assemblyFile;
         private static string assemblyName;
